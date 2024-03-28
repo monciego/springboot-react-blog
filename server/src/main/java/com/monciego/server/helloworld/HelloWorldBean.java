1bc963fb9cbd7f2fc6dcbea9fee86b2fa294ac26
monciego <jerichobantiquete@gmail.com>
2024-03-29 04:25:37 +0800

    enhancing rest api to return a bean

---
+++ server/src/main/java/com/monciego/server/helloworld/HelloWorldBean.java
@@ -0,0 +1,22 @@
+package com.monciego.server.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Hello World Bean [message=" + message +"]";
+    }
+}
--- server/src/main/java/com/monciego/server/helloworld/HelloWorldController.java
+++ server/src/main/java/com/monciego/server/helloworld/HelloWorldController.java
@@ -1,14 +1,18 @@
-package com.monciego.server;
+package com.monciego.server.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @RestController
 public class HelloWorldController {
-
-    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
