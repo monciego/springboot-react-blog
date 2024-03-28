4ec2187945809008a0f4962dec70d1b9042dd355
monciego <jerichobantiquete@gmail.com>
2024-03-29 05:46:03 +0800

    path parameter

--- server/src/main/java/com/monciego/server/helloworld/HelloWorldController.java
+++ server/src/main/java/com/monciego/server/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.monciego.server.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s",name));
     }
 }
